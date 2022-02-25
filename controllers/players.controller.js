078ee6c128f6174718f8c9aa09f8b527425e898e
raulrequenac <raulrequec@gmail.com>
2022-02-25 23:19:17 +0100

    players controller and routes added

--- controllers/players.controller.js
+++ controllers/players.controller.js
@@ -1,2 +1,19 @@
 const Player = require('../models/Player.model');
 
+module.exports.getAll = async(req, res, next) => {
+  try {
+    const players = Player.find();
+    res.status(200).json(players);
+  } catch (err) {
+    next(err)
+  }
+};
+
+module.exports.getById = async(req, res, next) => {
+  try {
+    const player = Player.findById(req.params.playerId);
+    res.status(200).json(player);
+  } catch (err) {
+    next(err)
+  }
+};
