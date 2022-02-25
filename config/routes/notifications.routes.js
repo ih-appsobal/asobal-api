6b2bd7a464b91a6b37913b541f2d9f167fd06a30
Juan Rodriguez Jimenez <juanorro@mac-juanorro.local>
2022-02-26 00:58:31 +0100

    notifications created and filter by user

--- config/routes/notifications.routes.js
+++ config/routes/notifications.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const notificationsController = require('../../controllers/notifications.controller');
+const { isAuthenticated } = require('../../middlewares/auth.middleware');
 
 router.post('/new', notificationsController.create);
+router.get('/:notificationId', isAuthenticated, notificationsController.userlist);
 
 module.exports = router;
--- controllers/notifications.controller.js
+++ controllers/notifications.controller.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const Notification = require('../models/notification.model');
+const User = require('../models/User.model');
 
 module.exports.create = async (req, res, next) => {
     try {
@@ -9,16 +10,26 @@ module.exports.create = async (req, res, next) => {
 
         res.status(200).json(notification);
     } catch (error) {
+
         next(error)
+
         console.log(error.message)
     }
 };
 
 //filter by user
 module.exports.userlist = async (req, res, next) => {
+
     try {
         
+        const notification = await Notification.find({ user: req.currentUser});
+
+        res.status(200).json(notification);
+
     } catch (error) {
         
-    }
-}
+        next(error);
+        console.log(error.message)
+
+    };
+};
