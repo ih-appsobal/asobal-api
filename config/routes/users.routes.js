d34d46ee93caa0de63cf42c7e3f3b04cfd4c6ec3
Marcos <marcos.monzon@alten.es>
2022-02-26 00:17:22 +0100

    feat: auth route

--- config/routes/users.routes.js
+++ config/routes/users.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../../controllers/users.controller');
 
-router.post('/', usersController.register)
+// Auth routes
+router.post('/login', usersController.authenticate)
+router.post('/users', usersController.register)
 
 module.exports = router;
