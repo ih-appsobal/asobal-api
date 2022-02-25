c0691f9facac6dea74eaf2d3e8c93b218a375f44
Pablo Laso <plasocortabitarte@gmail.com>
2022-02-25 20:57:01 +0100

    feat: update error messages

--- models/post.model.js
+++ models/post.model.js
@@ -1,34 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const PostSchema = Schema({
+const PostSchema = Schema(
+  {
     title: {
-        type: String,
-        require: [true, 'title-required']
-    }, 
+      type: String,
+      require: [true, "Introduce un título"],
+    },
     content: {
-        type: String,
-        require: [true, 'content-required']
+      type: String,
+      require: [true, "Introduce el contenido de tu post"],
     },
     media: {
-        type: String,
-        require: [true, 'media-required']
+      type: String,
+      require: [true, "Introduce un archivo multimedia"],
     },
     visible: {
-        type: Boolean,
-        require: [true, 'visible-required']
-    }
-}, {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
     timestamps: true,
     toJSON: {
-        virtuals: true,
-        transform: (doc, ret) => {
-            ret.id = doc._id;
-            delete ret._id;
-            delete ret.__v;
-            return ret;
-        }
-    }
-});
+      virtuals: true,
+      transform: (doc, ret) => {
+        ret.id = doc._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
 
-module.exports = mongoose.model('Post', PostSchema);
+module.exports = mongoose.model("Post", PostSchema);
