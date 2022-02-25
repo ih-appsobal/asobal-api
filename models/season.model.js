c28c097bb8c08f12ce290aae16dbe22ca2239c5e
juanorro <35376668+juanorro@users.noreply.github.com>
2022-02-25 22:20:31 +0100

    session model created (#6)
    * session model created
    
    * season model fix
    
    * hotfix
    
    Co-authored-by: Juan Rodriguez Jimenez <juanorro@mac-juanorro.local>
    Co-authored-by: raulrequenac <raulrequec@gmail.com>

---
+++ models/season.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const seasonSchema = Schema({
+    interval: {
+        type: String,
+        require: [true, 'El intervalo es obligatorio']
+    }
+}, {
+  timestamps:true,
+  toJSON: {
+    transform: (res, ret) => {
+      ret.id = res._id
+      delete ret._id
+      delete ret.__v
+      return ret
+    }
+  }
+});
+
+seasonSchema.virtual('matches', {
+    ref: 'Match',
+    localField: 'id',
+    foreignField: 'season',
+    justOne: false
+});
+
+seasonSchema.virtual('clubs', {
+    ref: 'Club',
+    localField: 'id',
+    foreignField: 'season',
+    justOne: false
+});
+
+const season = mongoose.model('Season', SeasonSchema);
+
+module.exports = season;
