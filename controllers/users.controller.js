c54b8e4e6f23c17842ecb7caabc7d34750ac9359
Marco Monzón <marco238cuba@gmail.com>
2022-02-26 00:20:14 +0100

    Feature/jwt (#14)
    feat: login

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,5 @@
-const createError = require('http-errors')
+const createError = require('http-errors');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User.model');
 
 module.exports.register = async (req, res, next) => {
@@ -14,4 +15,35 @@ module.exports.register = async (req, res, next) => {
   } catch(err) {
     next(err)
   }
+}
+
+module.exports.authenticate = (req, res, next) => {
+  const { email, password } = req.body
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        // Error if no user
+        next(createError(404, { errors: { email: 'Email or password is incorrect' }}))
+      } else {
+        return user.checkPassword(password)
+          .then(match => {
+            if (!match) {
+              //Error if no password
+              next(createError(404, { errors: { email: 'Email or password is incorrect' }}))
+            } else {
+              // JWT generation - only id is passed
+              res.json({ 
+                access_token: jwt.sign(
+                  { id: user._id },
+                  process.env.JWT_SECRET || 'changemeplease:v',
+                  {
+                    expiresIn: '1d'
+                  }
+                )
+               })
+            }
+          })
+      }
+    })
 }
