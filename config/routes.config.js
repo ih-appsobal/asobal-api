8972f459c8c5f44692c9991225a0214b19aabad8
Juan Rodriguez Jimenez <juanorro@mac-juanorro.local>
2022-02-25 22:11:33 +0100

    create and list posts

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const postController = require('../controllers/posts.controller');
+
 // Basic
-router.get('/', (req, res, next) => res.json({ response: "ok" }))
+router.get('/', (req, res, next) => res.json({ response: "ok" }));
+
+//posts
+router.post('/post', postController.create);
+router.get('/posts', postController.list);
 
 module.exports = router;
---
+++ controllers/posts.controller.js
@@ -0,0 +1,24 @@
+const Post = require('../models/post.model');
+
+module.exports.create = async(req, res) => {
+    
+    try {
+
+        const post = new Post(req.body);
+
+        await post.save();
+
+        res.status(200).json(post);
+
+    } catch (error) {
+
+        console.log(error.message);
+
+    };
+};
+
+module.exports.list = async(res) => {
+    const allPosts = await Post.find();
+
+    res.status(200).json(allPosts);
+};
