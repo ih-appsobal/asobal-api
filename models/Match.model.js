cb082be85c05a31b098036d7de0f881abee66155
raulrequenac <raulrequec@gmail.com>
2022-02-25 22:01:44 +0100

    Match model

---
+++ models/Match.model.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const goals = [{
+  type: Number,
+  required: [true, 'El minuto es requerido']
+}];
+
+const cards = [{
+  color: {
+    type: String,
+    enum: ['Amarilla', 'Roja', 'Azul'],
+    required: true
+  },
+  minute: {
+    type: Number,
+    required: [true, 'El minuto es requerido']
+  }
+}];
+
+const fouls = [{
+  type: Number,
+  required: [true, 'El minuto es requerido']
+}];
+
+const matchSchema = new Schema({
+  jornada: {
+    type: Number,
+    required: [true, 'La jornada es requerida']
+  },
+  date: {
+    type: Date,
+    required: [true, 'La fecha es requerida']
+  },
+  result: {
+    type: String,
+    enum: ['1', 'X', '2']
+  },
+  status: {
+    type: String,
+    enum: ['No comenzado', 'En vivo', 'Primera parte', 'Descanso', 'Segunda parte', 'Terminado'],
+    default: 'No comenzado'
+  },
+  local: {
+    club: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Club',
+      required: [true, 'El equipo local es requerido']
+    },
+    goals: goals,
+    cards: cards,
+    fouls: fouls
+  },
+  foreign: {
+    club: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Club',
+      required: [true, 'El equipo visitante es requerido']
+    },
+    goals: goals,
+    cards: cards,
+    fouls: fouls
+  },
+  minute: {
+    type: Number,
+    required: true,
+    default: 0,
+    min: 0
+  },
+  season: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Season',
+    required: [true, 'La temporada es obligatoria']
+  }
+});
+
+const Match = mongoose.model('Match', matchSchema);
+
+module.exports = Match;
