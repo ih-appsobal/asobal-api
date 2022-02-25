a6838235874eae32607e48b211ddfbda87a03dc9
Raul Requena <raulrequec@gmail.com>
2022-02-25 21:02:12 +0100

    Players and Clubs (#4)
    * Players and Clubs
    
    * cloudinary config updated & required error messages added
    
    * chore: names
    
    Co-authored-by: Pablo Laso <plasocortabitarte@gmail.com>

---
+++ models/Club.model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const clubSchema = new Schema({
+  logo: {
+    type: String,
+    required: [true, 'El logo es requerido']
+  },
+  name: {
+    type: String,
+    required: [true, 'El nombre es requerido']
+  },
+  rrss: [{
+    twitter: String,
+    instagram: String,
+    facebook: String,
+    youtube: String
+  }],
+  address: {
+    type: String,
+    required: [true, 'La dirección es requerida']
+  },
+  background: {
+    type: String,
+    default: String
+  }
+}, {
+  timestamps:true,
+  toJSON: {
+    transform: (res, ret) => {
+      ret.id = res._id
+      delete ret._id
+      delete ret.__v
+      return ret
+    }
+  }
+});
+
+clubSchema.virtual('players', {
+  ref: 'Player',
+  localField: 'id',
+  foreignField: 'club',
+  justOne: false
+});
+
+const club = new mongoose.model('Club', clubSchema);
+
+module.exports = club;
---
+++ models/Player.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const playerSchema = new Schema({
+  image: {
+    type: String,
+    required: true,
+    default: ''
+  },
+  name: {
+    type: String,
+    required: [true, 'El nombre es requerido']
+  },
+  rrss: [{
+    twitter: String,
+    instagram: String,
+    facebook: String,
+    youtube: String
+  }],
+  country: {
+    type: String,
+    required: [true, 'El país es requerido']
+  },
+  club: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Club',
+    required: [true, 'El club es requerido']
+  }
+}, {
+  timestamps:true,
+  toJSON: {
+    transform: (res, ret) => {
+      ret.id = res._id
+      delete ret._id
+      delete ret.__v
+      return ret
+    }
+  }
+});
+
+const player = new mongoose.model('Player', playerSchema);
+
+module.exports = player;
