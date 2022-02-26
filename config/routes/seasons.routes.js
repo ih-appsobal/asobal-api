7d1ed3f51cc3d560700c99f4676dbfbe41ee5ac0
raulrequenac <raulrequec@gmail.com>
2022-02-26 15:43:18 +0100

    fix

--- config/routes/seasons.routes.js
+++ config/routes/seasons.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const seasonsController = require('../../controllers/seasons.controller');
 
 router.get('/', seasonsController.getAll);
+router.get('/seasons', seasonsController.getMainSeason);
 router.get('/:seasonId', seasonsController.getById);
 
 module.exports = router;
