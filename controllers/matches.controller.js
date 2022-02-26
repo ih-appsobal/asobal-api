ce11a60ee67cc1ea6cd7ccd0875c27df47200f0e
Raul Requena <raulrequec@gmail.com>
2022-02-26 14:05:40 +0100

    Fixes (#34)
    * fixes
    
    * fixes
    
    * fix
    
    * fix
    
    * fix

--- controllers/matches.controller.js
+++ controllers/matches.controller.js
@@ -43,3 +43,24 @@ module.exports.getById = async(req, res, next) => {
     next(err)
   }
 };
+
+module.exports.getByFixture = async (req, res, next) => {
+  try {
+    const matches = await Match.find({fixture: req.params.fixture})
+      .populate({
+        path: 'local',
+        populate: {
+          path: 'club'
+        }
+      })
+      .populate({
+        path: 'foreign',
+        populate: {
+          path: 'club'
+        }
+      });
+    res.status(200).json(matches);
+  } catch (err) {
+    next(err);
+  }
+}
