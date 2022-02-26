36cb669849fc0239bc2582ff31bf0facb9c370b0
raulrequenac <raulrequec@gmail.com>
2022-02-26 14:04:45 +0100

    fix

--- config/routes/matches.routes.js
+++ config/routes/matches.routes.js
@@ -4,5 +4,6 @@ const matchesController = require('../../controllers/matches.controller');
 
 router.get('/', matchesController.getAll);
 router.get('/:matchId', matchesController.getById);
+router.get('/fixture/:fixture', matchesController.getByFixture);
 
 module.exports = router;
